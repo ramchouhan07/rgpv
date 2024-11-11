8d3738323696d10ab3003197517cac558d887dcb
ramchouhan07 <ramrajput53944@gmail.com>
2024-11-11 18:57:26 +0530

    first change

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Home from "../pages/Home"
+import Revolution from "../pages/revolution"
+import Chalange from "../pages/chalange"
+import { Route, Routes } from "react-router-dom"
+import Result from "../pages/RESULTPAGE"
+import Header from "../pages/Header"
+import Out from "../pages/out"
+function App() {
+
+  return (
+    <>
+     <Header />
+  
+  <Routes>
+    <Route  path="/" element={[<Home />,
+  <Revolution />,
+  <Chalange />]} />
+    <Route path="/out" element={<Out/>} />
+  
+    <Route path="/new-page" element={<Result />} />
+  </Routes>
+   </>
+  
+)
+}
+export default App
